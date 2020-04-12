01bc9f2c93451f59cd94579948125c33fab631e4
Ichisada Shioko <ichisadashioko@gmail.com>
2020-04-13 00:16:39 +0700

    :penguin: fix tkinter label example on linux

--- tkinter-samples/02_label.py
+++ tkinter-samples/02_label.py
@@ -1,23 +1,23 @@
 # encoding=utf-8
+from io import BytesIO
+
 import tkinter as tk
 import tkinter.font
+
 import requests
-from io import BytesIO
-from PIL import Image
-import numpy as np
-import matplotlib.pyplot as plt
+from PIL import Image, ImageTk
 
 app = tk.Tk()
 
-# basic label
+# """basic label"""
 # label = tk.Label(app, text='This is a Label')
 # label.pack()
 
-# more complicated labels
+# """more complicated labels"""
 # label_1 = tk.Label(app, text='Times 20', font=('Times', 20), bg='black', fg='white')
 # label_2 = tk.Label(app, text='Times 20 Italic', font=('Times', 20, 'italic'))
 
-# # font_3 = tk.font.Font(font=('Helvetica', 20, tk.font.BOLD))
+# font_3 = tk.font.Font(font=('Helvetica', 20, tk.font.BOLD))
 # font_3 = tk.font.Font(family='Helvetica', size=20, weight=tk.font.BOLD, underline=1, overstrike=1)
 # label_3 = tk.Label(app, text='Helvetica 20 bold underline overstrike', font=font_3)
 
@@ -25,10 +25,16 @@ app = tk.Tk()
 # label_2.pack()
 # label_3.pack()
 
-# display image with label
-# tk.PhotoImage only support GIF, PPM/PGM
-# image = tk.PhotoImage(file='<image-path>')
-# label = tk.Label(app, image=image)
-# labe.pack()
+"""display image with label"""
+
+sample_image_url = 'https://i.imgur.com/lIjvcCJ.jpg'
+res = requests.get(sample_image_url)
+image_filename = 'isekai_mahou.jpg'
+open(image_filename, mode='wb').write(res.content)
+
+"""tk.PhotoImage only support GIF, PPM/PGM"""
+image = ImageTk.PhotoImage(Image.open(image_filename))
+label = tk.Label(app, image=image)
+label.pack()
 
 app.mainloop()
