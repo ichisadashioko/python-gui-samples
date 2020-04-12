8cb289a10a11a08f1f9e22f64301bf1fe9460585
Ichisada Shioko <ichisadashioko@gmail.com>
2020-04-13 00:23:31 +0700

    :bug: re-visit tkinter button example

--- tkinter-samples/03_button.py
+++ tkinter-samples/03_button.py
@@ -3,37 +3,49 @@ from functools import partial
 
 app = tk.Tk()
 
-# basic button
-# btn1 = tk.Button(app, text='Button 1')
-# btn2 = tk.Button(app, text='Button 2')
+"""Example 1"""
+"""basic button"""
+btn1 = tk.Button(app, text='Button 1')
+btn2 = tk.Button(app, text='Button 2')
 
-# btn1.pack(side=tk.LEFT)
-# btn2.pack(side=tk.RIGHT)
+btn1.pack(side=tk.LEFT)
+btn2.pack(side=tk.RIGHT)
 
-# callback
-# label = tk.Button(app, text='0')
-
-# def label_counter():
-#     count = int(str(label['text']))
-#     count += 1
-#     label.config(text=str(count))
-
-# btn = tk.Button(app, text='increase', width=30, command=label_counter)
-
-# btn.pack()
-# label.pack()
-
-# callback with arguments
 label = tk.Button(app, text='0')
 
-def label_counter(num: int):
+"""event callback"""
+def label_counter():
     count = int(str(label['text']))
-    count += num
+    count += 1
     label.config(text=str(count))
 
-btn = tk.Button(app, text='increase', width=30, command=partial(label_counter, 2))
+btn = tk.Button(app, text='increase', width=30, command=label_counter)
 
 btn.pack()
 label.pack()
+"""End example 1"""
+
+
+# """Example 2"""
+# """callback with arguments"""
+# label = tk.Button(app, text='0')
+
+
+# def label_counter(num: int):
+#     count = int(str(label['text']))
+#     count += num
+#     label.config(text=str(count))
+
+
+# btn = tk.Button(
+#     app,
+#     text='increase',
+#     width=30,
+#     command=partial(label_counter, 2),
+# )
+
+# btn.pack()
+# label.pack()
+# """End example 2"""
 
 app.mainloop()
